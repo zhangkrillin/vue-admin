d8ae901c2dbe6a8736549a9ca29553c49287c107
zhangkrillin <401385571@qq.com>
2019-11-09 10:35:50 +0800

    menulist

--- src/main.js
+++ src/main.js
@@ -7,11 +7,17 @@ import "./assets/css/global.css"
 import "./assets/fonts/iconfont.css"
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  // console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
 new Vue({
-    router,
-    render: h => h(App)
+  router,
+  render: h => h(App)
 }).$mount('#app')
